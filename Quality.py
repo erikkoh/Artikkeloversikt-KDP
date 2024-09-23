8684ca04ff321f3535cd0244f57a7a4ed05b4217
Kristoffer <e.k.ohlckers@gmail.com>
2024-09-23 13:46:21 +0200

    added quality rank aswell

--- Quality.py
+++ Quality.py
@@ -5,33 +5,61 @@ from datetime import datetime
 import pandas as pd
 
 # Link til artikeloversikt på snl
-link = urllib.request.Request("https://snl.no/.taxonomy/56",)
+link = urllib.request.Request(
+    "https://snl.no/.taxonomy/56",
+)
+
+
+session_cookie = "__session_id=" + str(input("Provide a session cookie: "))
+
 
 with urllib.request.urlopen(link) as raw:
-    soup = BeautifulSoup(raw.read().decode('utf-8'), 'html.parser')
+    soup = BeautifulSoup(raw.read().decode("utf-8"), "html.parser")
 
-articles = soup.find_all("a", class_ = 'link-list__link')
+articles = soup.find_all("a", class_="link-list__link")
 article_names = [(x.string).lower() for x in articles]
 article_dic = {}
 
-#getting all articles
-article_links = [x.get('href') for x in articles]
+
+# getting all articles
+article_links = [article.get("href") for article in articles]
 for i in range(0, len(article_names)):
-    with urllib.request.urlopen(article_links[i]) as raw:
-        soup = BeautifulSoup(raw.read().decode('utf-8'), 'html.parser')
-    article_info= soup.find(
-        class_ = "l-article__meta")
-    #It works but looks awful
-    multiple_timeformat = article_info.find(
-        'time', {'datetime' : True})
-    article_dic[article_names[i]] = [multiple_timeformat['datetime'], (multiple_timeformat.string).lstrip()]
-print(article_dic)
+    article_request = urllib.request.Request(article_links[i])
+    article_request.add_header("Cookie", session_cookie)
+    with urllib.request.urlopen(article_request) as raw:
+        soup = BeautifulSoup(raw.read().decode("utf-8"), "html.parser")
+    article_info = soup.find(class_="l-article__meta")  # info box where time is found
+    article_quality = (
+        (soup.find(class_="quality-assessment__existing-heading")).find("span").string
+    )
+
+    # It works but looks awful
+    multiple_timeformat = article_info.find("time", {"datetime": True})
+    article_dic[article_names[i]] = {
+        "Raw time": multiple_timeformat["datetime"],
+        "Norwegian time": (multiple_timeformat.string).lstrip(),
+        "Quality": article_quality.lstrip(),
+    }
 
 
-article_dic = dict(sorted(article_dic.items(), key=lambda item: datetime.strptime(item[1][0], "%Y-%m-%dT%H:%M:%S%z")))
-article_dic = {k: v[1] for k, v in article_dic.items()}
-print(article_dic)
+article_dic = dict(
+    sorted(
+        article_dic.items(),
+        key=lambda item: datetime.strptime(item[1]["Raw time"], "%Y-%m-%dT%H:%M:%S%z"),
+    )
+)
+for v in article_dic.values():
+    v.pop("Raw time", None)
 
-df = pd.DataFrame(article_dic.items(), columns=["Artikkel", "Sist oppdatert"])
+article_names = list(article_dic.keys())
+norwegian_times = [v["Norwegian time"] for v in article_dic.values()]
+qualities = [v["Quality"] for v in article_dic.values()]
 
-df.to_excel('data/Siste_oppdatert_artikler.xlsx', index=False)
+df = pd.DataFrame(
+    {
+        "Artikkel": article_names,
+        "Sist oppdatert": norwegian_times,
+        "Kvalitet": qualities,
+    }
+)
+df.to_excel("data/Siste_oppdatert_artikler.xlsx", index=False)
