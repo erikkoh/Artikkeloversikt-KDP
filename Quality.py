43bf86c39149da9a23c6c279b62b0efd966db7c0
ChristianGynnild <75206913+ChristianGynnild@users.noreply.github.com>
2024-11-05 11:50:43 +0100

    Include section for reader count for all time and last year for Quality.py

--- Quality.py
+++ Quality.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import matplotlib.pyplot as plt
+
 from datetime import datetime
 import pandas as pd
 
@@ -32,6 +32,9 @@ for i in range(0, len(article_names)):
     article_quality = (
         (soup.find(class_="quality-assessment__existing-heading")).find("span").string
     )
+    reader_counts = soup.find_all("p",class_="read-stats__count")
+    reader_count_last_year = reader_counts[0].string.strip()
+    reader_count_all_time = reader_counts[1].string.strip()
 
     # It works but looks awful
     multiple_timeformat = article_info.find("time", {"datetime": True})
@@ -39,6 +42,8 @@ for i in range(0, len(article_names)):
         "Raw time": multiple_timeformat["datetime"],
         "Norwegian time": (multiple_timeformat.string).lstrip(),
         "Quality": article_quality.lstrip(),
+        "Reader count last year": reader_count_last_year,
+        "Reader count all time": reader_count_all_time,
     }
 
 
@@ -54,12 +59,17 @@ for v in article_dic.values():
 article_names = list(article_dic.keys())
 norwegian_times = [v["Norwegian time"] for v in article_dic.values()]
 qualities = [v["Quality"] for v in article_dic.values()]
+reader_counts_last_year = [v["Reader count last year"] for v in article_dic.values()]
+reader_counts_all_time = [v["Reader count all time"] for v in article_dic.values()]
 
 df = pd.DataFrame(
     {
         "Artikkel": article_names,
         "Sist oppdatert": norwegian_times,
         "Kvalitet": qualities,
+        "Lest siste år": reader_counts_last_year,
+        "Lest siden 2015": reader_counts_all_time,
     }
 )
+
 df.to_excel("data/Siste_oppdatert_artikler.xlsx", index=False)
